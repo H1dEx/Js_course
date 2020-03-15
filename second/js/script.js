fd03b999705b5677d867b61c66196d598a27fc3a
H1dEx <hidex404@gmail.com>
2020-03-15 18:57:23 +0300

    promises was added

--- second/js/script.js
+++ second/js/script.js
@@ -105,4 +105,63 @@ window.addEventListener('DOMContentLoaded', () => {
         document.body.style.overflow = "";
         modalBtn.classList.remove('more-splash');
     });
+
+    //form
+
+    let message = {
+        loading: 'Загрузка ...',
+        success: 'Спасибо, скоро мы с вами свяжемся!',
+        failure: 'Что-то пошло не так...'
+    };
+
+    let form = document.querySelector('.main-form'),
+        statusMessage = document.createElement('div'),
+        contactForm = document.querySelector('#form');
+
+    statusMessage.classList.add('status');
+
+    form.addEventListener('submit', sendRequest);
+    contactForm.addEventListener('submit', sendRequest);
+
+    function sendRequest(e) {
+        e.preventDefault();
+        this.appendChild(statusMessage);
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+        let formData = new FormData(this),
+            input = this.getElementsByTagName('input'),
+            obj = {};
+
+        formData.forEach(function (value, key) {
+            obj[key] = value;
+        });
+
+        let json = JSON.stringify(obj);
+
+        request.send(json);
+
+        function getStatus() {
+            return new Promise((resolve, reject) => {
+                request.addEventListener('readystatechange', () => {
+                    if (request.readyState < 4) {
+                        resolve();
+                        // statusMessage.innerHTML = message.loading;
+                    } else if (request.readyState == 4 && request.status == 200) {
+                        resolve();
+                        // statusMessage.innerHTML = message.success;
+                    } else reject();
+                    // statusMessage.innerHTML = message.failure;
+                    for (let i = 0; i < input.length; i++)
+                        input[i].value = '';
+                });
+            });
+        }
+        getStatus()
+            .then(() => statusMessage.innerHTML = message.loading)
+            .then(() => statusMessage.innerHTML = message.success)
+            .catch(() => statusMessage.innerHTML = message.failure);
+    }
+
 });
